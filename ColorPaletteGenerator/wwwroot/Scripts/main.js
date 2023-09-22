26577b339accaa6d41de5e13f797e1643b995216
Rick Corsi <cors9056@fredonia.edu>
2023-09-22 00:54:25 -0400

    - Added Delete Functionality - Added RGB Value Display

--- ColorPaletteGenerator/wwwroot/Scripts/main.js
+++ ColorPaletteGenerator/wwwroot/Scripts/main.js
@@ -8,7 +8,7 @@ function openColorSelect(c) {
     console.log(c);
     var s = document.getElementById("selectedColor");
     s.style.backgroundColor = c;
-    var t = document.getElementById("selectedColorText");
+    var t = document.getElementById("selectedColorTextHEX");
     t.innerHTML = c;
 
     var str = c;
@@ -27,25 +27,48 @@ function cloneDiv() {
     const objects = document.querySelectorAll("#inputArea > div");
     console.log("Divs: " + objects.length);
     console.log("Objs: " + objs);
+    const originalDiv = document.getElementById('selectedColor');
+    const deleteButton = document.getElementById('selectedColorDelete');
 
-    var container = document.getElementById('favorites');
-    var original = document.getElementById('selectedColor');
+    const cloneDiv = originalDiv.cloneNode(true);
+    const cloneButton = deleteButton.cloneNode(true);
 
-    clone = original.cloneNode(true);
-    clone.id = "clonedDiv_" + objs;
-    clone.textContent = document.getElementById('selectedColorText').textContent;
-    var hex = document.getElementById('selectedColorText').textContent;
+    const uniqueId = "clonedDiv_" + objs;
+
+    cloneDiv.id = uniqueId;
+    cloneButton.id = "clonedDelete_" + objs;
+
+    cloneDiv.textContent = document.getElementById('selectedColorTextHEX').textContent;
+    var hexValue = document.getElementById('selectedColorTextHEX').textContent;
+    var rgbValue = hexToRgb(hexValue); // Calculate RGB from HEX
+
+    // Set the text content to display both HEX and RGB values
+    cloneDiv.textContent = `${hexValue}, ${rgbValue}`;
+
+
+    var hex = document.getElementById('selectedColorTextHEX').textContent;
     checkBrightness(hex);
     const base = 127.5;
     if (checkBrightness(hex) < base) {
-        clone.style.color = "white";
+        cloneDiv.style.color = "white";
     }
     else {
-        clone.style.color = "black";
+        cloneDiv.style.color = "black";
     }
-    container.appendChild(clone);
+
+    cloneButton.addEventListener('click', function () {
+        const divToDelete = document.getElementById(uniqueId);
+        if (divToDelete) {
+            divToDelete.remove();
+            console.log("Object Unfavorited");
+        }
+    });
+    const container = document.getElementById('favorites');
+    container.appendChild(cloneDiv);
+    cloneDiv.appendChild(cloneButton);
     objs++;
     console.log("clone.id: " + clone.id);
+    console.log("cloneDelete.id: " + cloneDelete.id);
 }
 function checkBrightness(color) {
     const hex = color.replace('#', '');
@@ -55,4 +78,16 @@ function checkBrightness(color) {
     const brightness = ((c_r * 299) + (c_g * 587) + (c_b * 114)) / 1000;
     console.log("brightness: " + brightness);
     return brightness;
+}
+function hexToRgb(hex) {
+    // Remove the hash (#) character if it exists
+    hex = hex.replace(/^#/, '');
+
+    // Parse the hexadecimal value to RGB components
+    const bigint = parseInt(hex, 16);
+    const r = (bigint >> 16) & 255;
+    const g = (bigint >> 8) & 255;
+    const b = bigint & 255;
+
+    return `RGB(${r}, ${g}, ${b})`;
 }
