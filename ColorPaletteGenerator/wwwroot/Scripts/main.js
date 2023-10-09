2fa15c2107e87000d0eea47a3de783144bf3aded
Rick Corsi <cors9056@fredonia.edu>
2023-10-08 23:47:40 -0400

    - Copy Color Button Functionality - Favorite Color Button Functionality - Download Palette - New Animations and Transitiions - Color Names using thecolorapi - Random Colors on Page Load - Clear all Favorites

--- ColorPaletteGenerator/wwwroot/Scripts/main.js
+++ ColorPaletteGenerator/wwwroot/Scripts/main.js
@@ -1,12 +1,40 @@
-﻿function hideInfo() {
+﻿async function getColorName(hexValue) {
+    console.error('Getting Color Name... ');
+    try {
+        // Encode the hex value to ensure it's safe for the URL
+        const encodedHex = encodeURIComponent(hexValue);
+
+        // Make a GET request to the Color API
+        const response = await fetch(`https://www.thecolorapi.com/id?hex=${encodedHex}&format=json`);
+
+        // Check if the response status is OK (200)
+        if (response.status === 200) {
+            // Parse the response JSON
+            const data = await response.json();
+
+            // Extract the color name from the response
+            const colorName = data.name.value;
+
+            return colorName;
+        } else {
+            console.error('Failed to retrieve color information.');
+            return null;
+        }
+    } catch (error) {
+        console.error('An error occurred while fetching color information:', error);
+        return null;
+    }
+}
+function hideInfo() {
     var divsToHide = document.getElementsByClassName("paletteinfo");
     for (var i = 0; i < divsToHide.length; i++) {
         divsToHide[i].style.height = "0px"; 
     }
 }
-function openColorSelect(c) {
+function openColorSelect(c, j) {
     console.log(c);
     var s = document.getElementById("selectedColor");
+    const b = document.getElementById("paletteFav_" + j);
     //var l = document.getElementById("selectedColorSmall");
     s.style.backgroundColor = c;
     //l.style.backgroundColor = c;
@@ -15,14 +43,19 @@ function openColorSelect(c) {
 
     var str = c;
     var firstTwoChars = str.slice(1, 2);
-    console.log(firstTwoChars);
     t.style.color = "black";
     for (var i = 0; i < 9; i++) {
         if (firstTwoChars == i) {
             t.style.color = "white";
         }
     }
+    b.style.paddingTop = "10px";
     cloneDiv();
+    setTimeout(function () {
+        // Code to execute after the pause (2 seconds)
+        // Continue with your code here
+        b.style.paddingTop = "0px";
+    }, 2000); // 2000 milliseconds (2 seconds)
 //    cloneDivSmall();
 }
 var objs = 1;
@@ -32,13 +65,11 @@ function cloneDiv() {
     const deleteButton = document.getElementById('selectedColorDelete');
     const expandButton = document.getElementById('selectedColorExpand');
     const copyButton = document.getElementById('selectedColorCopy');
-    const pickButton = document.getElementById('selectedColorPick');
 
     const cloneDiv = originalDiv.cloneNode(true);
     const cloneButtonDelete = deleteButton.cloneNode(true);
     const cloneButtonExpand = expandButton.cloneNode(true);
     const cloneButtonCopy = copyButton.cloneNode(true);
-    const cloneButtonPick = pickButton.cloneNode(true);
 
     const uniqueId = "clonedDiv_" + objs;
 
@@ -46,15 +77,21 @@ function cloneDiv() {
     cloneButtonDelete.id = "clonedDelete_" + objs;
     cloneButtonExpand.id = "clonedExpand_" + objs;
     cloneButtonCopy.id = "clonedCopy_" + objs;
-    cloneButtonPick.id = "clonedPick_" + objs;
 
     cloneDiv.textContent = document.getElementById('selectedColorTextHEX').textContent;
     var hexValue = document.getElementById('selectedColorTextHEX').textContent;
     var rgbValue = hexToRgb(hexValue); // Calculate RGB from HEX
     var hslValue = hexToHsl(hexValue); // Calculate RGB from HEX
-    console.log("hslValue: " + hslValue);
-    // Set the text content to display both HEX and RGB values
-    cloneDiv.innerHTML = `${hexValue} <br>${rgbValue} <br>${hslValue}`;
+    var nameValue = ""; // Initialize the global variable
+
+    getColorName(hexValue)
+     .then(name => { // Use a different variable name here
+        nameValue = name; // Assign the value to the global variable
+        console.log(`Color Name: ${nameValue}`);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
 
     var hex = document.getElementById('selectedColorTextHEX').textContent;
     checkBrightness(hex);
@@ -81,27 +118,33 @@ function cloneDiv() {
         const c = document.getElementById("classPick");
         const f = document.getElementById("favsText");
         const h = document.getElementById("fullColorHEX");
+        h.style.color = "transparent";
         x.style.backgroundColor = divToExpand.style.backgroundColor;
         x.style.height = "80%";
         b.style.display = "block";
         d.style.display = "block";
         c.style.right = "600%";
         f.style.marginTop = "700px";
-        h.innerHTML = `${hexValue} <br>${rgbValue} <br>${hslValue}`;
-        hex = divToExpand.textContent;
+        hex = hexValue;
         console.log("hex: " + hex);
         checkBrightness(hex);
         const base = 127.5;
         if (checkBrightness(hex) < base) {
             b.style.color = "white";
             d.style.color = "white";
-            h.style.color = "white";
+            setTimeout(function () {
+                h.innerHTML = `${nameValue} <br>${hexValue} <br>${rgbValue} <br>${hslValue}`;
+                h.style.color = "white";
+            }, 750);
             f.style.color = "white";
         }
         else {
             b.style.color = "black";
             d.style.color = "black";
-            h.style.color = "black";
+            setTimeout(function () {
+                h.innerHTML = `${nameValue} <br>${hexValue} <br>${rgbValue} <br>${hslValue}`;
+                h.style.color = "black";
+            }, 750);
             f.style.color = "black";
         }
         window.scrollTo({ top: 0, behavior: 'smooth' });
@@ -139,18 +182,31 @@ function cloneDiv() {
             cloneButtonCopy.style.paddingTop = "0px";
         }, 2000); // 2000 milliseconds (2 seconds)
     });
-    cloneButtonPick.addEventListener('click', function () {
-        const mudpick = document.getElementById('mudpick'); // Get the MudColorPicker element by ID
-        mudpick.value = '#FF0000'; // Change the color value to your desired value
-        console.log("Hello");
-    });
+
     const container = document.getElementById('favorites');
-    container.appendChild(cloneDiv);
-    cloneDiv.appendChild(cloneButtonExpand);
-    cloneDiv.appendChild(cloneButtonDelete);
-    cloneDiv.appendChild(cloneButtonCopy);
-    cloneDiv.appendChild(cloneButtonPick);
+
     objs++;
+    const alertText = document.getElementById("alert");
+    const alertContainer = document.getElementById("alertContain");
+
+    alertContainer.style.left = "15px";
+    alertContainer.style.backgroundColor = hexValue;
+    alertText.textContent = `Favorited! ${hexValue}`;
+    checkBrightness(hexValue);
+    if (checkBrightness(hexValue) < base) {
+        alertText.style.color = "white";
+    }
+    else {
+        alertText.style.color = "black";
+    }
+    setTimeout(function () {
+        alertContainer.style.left = "-100%";
+        cloneDiv.innerHTML = `${nameValue} <br>${hexValue} <br>${rgbValue} <br>${hslValue}`;
+        container.appendChild(cloneDiv);
+        cloneDiv.appendChild(cloneButtonExpand);
+        cloneDiv.appendChild(cloneButtonDelete);
+        cloneDiv.appendChild(cloneButtonCopy);
+    }, 2000);
 }
 function cloneDivSmall() {
     console.log("Small Div Cloned");
@@ -173,7 +229,6 @@ function checkBrightness(color) {
     const c_g = parseInt(hex.substr(2, 2), 16);
     const c_b = parseInt(hex.substr(4, 2), 16);
     const brightness = ((c_r * 299) + (c_g * 587) + (c_b * 114)) / 1000;
-    console.log("brightness: " + brightness);
     return brightness;
 }
 function hexToRgb(hex) {
@@ -187,6 +242,11 @@ function hexToRgb(hex) {
     const b = bigint & 255;
 
     return `RGB(${r}, ${g}, ${b})`;
+}
+function rgbToHex(rgb) {
+    const values = rgb.match(/\d+/g);
+    const hexColor = '#' + values.map(val => parseInt(val).toString(16).padStart(2, '0')).join('');
+    return hexColor;
 }
 function hexToHsl(hex) {
     // Remove the hash (#) character if it exists
@@ -246,31 +306,35 @@ function toggleFavoritesView() {
     console.log("Button Toggled");
 }
 function closePreview() {
-    const x = document.getElementById("fullColor");
-    const b = document.getElementById("selectedColorExpandClose");
-    const d = document.getElementById("selectedColorExpandDownload");
-    const c = document.getElementById("classPick");
-    const f = document.getElementById("favsText");
-    const h = document.getElementById("fullColorHEX");
-    x.style.backgroundColor = "#ffffff";
-    x.style.height = "0px";
-    b.style.display = "none";
-    d.style.display = "none";
-    c.style.right = "0%";
-    f.style.marginTop = "15px";
-    f.style.color = "black";
-    h.textContent = '';
+    const fullColorElement = document.getElementById("fullColor");
+    const selectedColorExpandCloseElement = document.getElementById("selectedColorExpandClose");
+    const selectedColorExpandDownloadElement = document.getElementById("selectedColorExpandDownload");
+    const classPickElement = document.getElementById("classPick");
+    const favsTextElement = document.getElementById("favsText");
+    const fullColorHEXElement = document.getElementById("fullColorHEX");
+
+    fullColorElement.style.backgroundColor = "#ffffff";
+    fullColorElement.style.height = "0px";
+    selectedColorExpandCloseElement.style.display = "none";
+    selectedColorExpandDownloadElement.style.display = "none";
+    classPickElement.style.right = "0%";
+    favsTextElement.style.marginTop = "15px";
+    favsTextElement.style.color = "black";
+    fullColorHEXElement.textContent = '';
     document.body.style.backgroundColor = "#ffffff";
+
 }
 function downloadPreview() {
 
     // Get a reference to the <body> element
     const bodyElement = document.body;
-
+    const rgb = document.body.style.backgroundColor;
+    const filename = rgbToHex(rgb);
     const maxHeight = 800; // Set your desired maximum height in pixels
     const maxWidth = 1400; // Set your desired maximum height in pixels
     const elementsToIgnore = document.querySelectorAll('#selectedColorExpandDownload'); // Replace with your selectors
-
+    const downloadButton = document.getElementById("selectedColorExpandDownload");
+    downloadButton.style.paddingTop = "10px";
     html2canvas(bodyElement, {
         width: maxWidth,
         height: maxHeight,
@@ -281,30 +345,166 @@ function downloadPreview() {
         // Create a download link for the image
         const downloadLink = document.createElement('a');
         downloadLink.href = dataURL;
-        downloadLink.download = 'colorbackground.png'; // Specify the desired filename
+        downloadLink.download = (filename + '.png'); // Specify the desired filename
 
         // Trigger a click event on the download link to prompt the user to download the image
-        downloadLink.click();    });
+        downloadLink.click();
+    });
+    setTimeout(function () {
+        downloadButton.style.paddingTop = "0px";
+    }, 2000);
 }
 function toggleHide() {
     const hideme = document.getElementById('fullColorHEX');
-    let tempcolor = "white";
-    const back = document.body.style.backgroundColor;
-
-    console.log("Back:" + back)
-
+    let tempColor = "white";
+    const bodyBackgroundColor = document.body.style.backgroundColor;
+    const hexBackgroundColor = rgbToHex(bodyBackgroundColor);
     const base = 127.5;
-    if (checkBrightness(back) < base) {
-        tempcolor = "white";
+
+    if (checkBrightness(hexBackgroundColor) < base) {
+        tempColor = "white";
     }
     else {
-        tempcolor = "black";
+        tempColor = "black";
     }
 
     if (hideme.style.color == "transparent") {
-        hideme.style.color = tempcolor;
+        hideme.style.color = tempColor;
     }
     else {
         hideme.style.color = "transparent";
     }
+}
+function copyColorSelect(hexValue, index) {
+    console.log(hexValue);
+    const tempInput = document.createElement("input");
+    const alertElement = document.getElementById("alert");
+    const paletteCopyElement = document.getElementById("paletteCopy_" + index);
+    const alertContainElement = document.getElementById("alertContain");
+
+    tempInput.value = hexValue;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
+
+    alertContainElement.style.left = "15px";
+    alertContainElement.style.backgroundColor = hexValue;
+    paletteCopyElement.style.paddingTop = "10px";
+    alertElement.textContent = `Copied! ${hexValue}`;
+
+    checkBrightness(hexValue);
+    const base = 127.5;
+
+    if (checkBrightness(hexValue) < base) {
+        alertElement.style.color = "white";
+    } else {
+        alertElement.style.color = "black";
+    }
+
+    setTimeout(function () {
+        alertContainElement.style.left = "-100%";
+        paletteCopyElement.style.paddingTop = "0px";
+    }, 2000);
+}
+function testColorName() {
+    const hexValue = '#0047AB';
+    getColorName(hexValue)
+        .then((colorName) => {
+            if (colorName) {
+                console.log(`The color name for ${hexValue} is ${colorName}`);
+            } else {
+                console.log('Color name not found.');
+            }
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
+function closeAll() {
+    const clearButton = document.getElementById("clearFavs");
+    const favoritesDiv = document.getElementById("favorites");
+
+    clearButton.style.paddingTop = "10px";
+
+    if (favoritesDiv.children.length === 0) {
+        runAlert('No favorites to clear!', 2000);
+        setTimeout(function () {
+            clearButton.style.paddingTop = "0px";
+        }, 2000);
+        return; // Exit the function
+    }
+    runAlert('Favorites Cleared!', 2000);
+
+    setTimeout(function () {
+        while (favoritesDiv.firstChild) {
+            favoritesDiv.removeChild(favoritesDiv.firstChild);
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+        }
+        clearButton.style.paddingTop = "0px";
+    }, 500);
+
+}
+function saveFavorites() {
+    const saveButton = document.getElementById("saveFavs");
+    const favoritesDiv = document.getElementById('favorites');
+
+    saveButton.style.paddingTop = "10px";
+
+    if (favoritesDiv.children.length === 0) {
+        runAlert('No favorites to save!', 2000);
+        setTimeout(function () {
+            saveButton.style.paddingTop = "0px";
+        }, 2000);
+        return; // Exit the function
+    }
+
+    const maxWidth = 400; // Set your desired maximum height in pixels
+
+    html2canvas(favoritesDiv, {
+        width: maxWidth,
+    }).then(canvas => {
+        const dataURL = canvas.toDataURL('image/png');
+
+        // Create a download link for the image
+        const downloadLink = document.createElement('a');
+        downloadLink.href = dataURL;
+        downloadLink.download = 'palette.png'; // Specify the desired filename
+
+        // Trigger a click event on the download link to prompt the user to download the image
+        downloadLink.click();
+    });
+
+    runAlert('Palette Downloaded!', 2000);
+    setTimeout(function () {
+        saveButton.style.paddingTop = "0px";
+    }, 2000);
+}
+function runAlert(message, duration) {
+    const alertContainer = document.getElementById("alertContain");
+    const alertText = document.getElementById("alert");
+    const defaultPaletteColor = document.getElementById("paletteColorDefault");
+
+    // Set container's initial position and background color
+    alertContainer.style.left = "15px";
+    alertContainer.style.backgroundColor = defaultPaletteColor.style.backgroundColor;
+
+    // Set alert message text
+    alertText.textContent = message;
+
+    // Get and check the brightness of the background color
+    const hexBackgroundColor = rgbToHex(alertContainer.style.backgroundColor);
+    const baseBrightness = 127.5;
+
+    if (checkBrightness(hexBackgroundColor) < baseBrightness) {
+        alertText.style.color = "white";
+    } else {
+        alertText.style.color = "black";
+    }
+
+    // Hide the alert after the specified duration
+    setTimeout(function () {
+        alertContainer.style.left = "-100%";
+        alertText.style.paddingTop = "0px";
+    }, duration);
 }
